9571aa3acc297b8f2c8a06b99ab851d746199c9f
Felipe Félix <felipefelixsdl@gmail.com>
2025-05-01 20:27:51 -0300

    Remove configuração do banco de dados e rota de teste do aplicativo Flask

--- app.py
+++ app.py
@@ -4,20 +4,12 @@ from config import Config
 from sqlalchemy import text
 
 app = Flask(__name__)
-app.config.from_object(Config)
-db = SQLAlchemy(app)
 
 @app.route("/")
 def hello():
     return "Olá mundo!"
 
-@app.route("/test-db")
-def test():
-    try:
-        db.session.execute(text("SELECT 1"))
-        return "Banco de dados conectado!"
-    except Exception as e:
-        return f"Erro ao conectar ao banco de dados: {e}"       
-
 if __name__ == "__main__":
-    app.run() 
+    app.run(
+        debug=True,
+    ) 
