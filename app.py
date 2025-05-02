5349d00da771a5c67fc33a358d4ebc89a0653f72
Felipe Félix <felipefelixsdl@gmail.com>
2025-05-01 21:07:00 -0300

    Atualiza a rota inicial para renderizar o template index.html e adiciona o arquivo HTML correspondente

--- app.py
+++ app.py
@@ -1,13 +1,10 @@
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from config import Config
-from sqlalchemy import text
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "Olá mundo!"
+def home():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app.run(
