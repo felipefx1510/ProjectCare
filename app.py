fdee9dad2f8ea1927babbf96bb51958bbe55b201
Felipe Félix <felipefelixsdl@gmail.com>
2025-05-02 19:43:01 -0300

    Adiciona nova rota para categorias, atualiza o template index.html com links para gerenciamento de categorias e clientes, e implementa o arquivo categories.html. Cria o fragmento _navbar.html para a barra de navegação.

--- app.py
+++ app.py
@@ -10,5 +10,11 @@ db = SQLAlchemy(app)
 def home():
     return render_template("index.html")
 
+@app.route("/categories")
+def categories():
+    from entities.category import Category
+    categories = Category.query.all()
+    return render_template("categories.html", categories=categories)
+
 if __name__ == "__main__":
     app.run(debug=True)
