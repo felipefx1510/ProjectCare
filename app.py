37b005c6952d526548d6bfb199bab55925ce7d65
Felipe Félix <felipefelixsdl@gmail.com>
2025-05-02 20:21:03 -0300

    Atualiza a estrutura de rotas e templates para gerenciamento de categorias, adiciona páginas de registro e lista de categorias, e remove arquivos desnecessários.

--- app.py
+++ app.py
@@ -8,13 +8,21 @@ db = SQLAlchemy(app)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("home.html")
 
 @app.route("/categories")
 def categories():
-    from entities.category import Category
-    categories = Category.query.all()
-    return render_template("categories.html", categories=categories)
+    return render_template("categories/categories.html")
+
+@app.route("/categories/register")
+def register_categorie():
+    return render_template("categories/register.html")
+
+@app.route("/categories/register/save", methods=["POST"])
+def save_categorie():
+    # Aqui você pode adicionar a lógica para salvar a categoria no banco de dados
+    return render_template("categories/categories.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
