e5ea3426f9a0a39af0164541acc64050be31df60
Felipe Félix <felipefelixsdl@gmail.com>
2025-05-01 21:08:08 -0300

    Remove importações não utilizadas do arquivo app.py

--- app.py
+++ app.py
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
 from flask import Flask, render_template
-=======
-from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import text
->>>>>>> ee7f7ed0c13ecd43596e39693f8de18364c0176f
-
 app = Flask(__name__)
 
 @app.route("/")
