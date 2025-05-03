d7c3583bd611aad8489e7998640f1c01f6b65713
Felipe Félix <felipefelixsdl@gmail.com>
2025-05-03 04:59:24 -0300

    Comenta importações e classe de configuração no arquivo config.py

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
-from dotenv import load_dotenv
-import os
+# from dotenv import load_dotenv
+# import os
 
-load_dotenv()
+# load_dotenv()
 
-class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+# class Config:
+#     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
